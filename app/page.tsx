b154bad1ea91f5a0a670dc453b86aa92f74cc9a4
walid hamdy44 <walidhamdy314@gmail.com>
2024-08-03 15:43:26 +0300

    update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { Toggle } from "@/components/toggle";
+import NavBar from "@/components/landing/NavBar";
 
 export default function Home() {
-  return <div>{/* <Toggle /> */}</div>;
+  return (
+    <div className="min-h-[1000px]">
+      <div className="landing">
+        <NavBar />
+        {/* <Hero /> */}
+      </div>
+    </div>
+  );
 }
